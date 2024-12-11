743c037ad1b3f3a8d44a70090d683eaa6e0a5208
gurunglaxmi891 <gurunglaxmi891@gmail.com>
2024-12-11 16:35:15 +0545

    scenario code stepDifinition test

--- tests/stepDefinitions/loginContext.js
+++ tests/stepDefinitions/loginContext.js
@@ -1,15 +1,16 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
-Given('user has navigated to the login page', function () {
+
+Given('the user has navigated to the login page', function () {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    console.log("hello");
   });
 
   When('user logs in with email {string} and password {string}', function (string, string2) {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    console.log("hello");
   });
 
   Then('user should be navigated to home page', function () {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    console.log("hello");
   });
