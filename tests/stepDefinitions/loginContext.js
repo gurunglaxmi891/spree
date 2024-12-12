420d59f97a274655d54e970db4e46c6a633c134d
gurunglaxmi891 <gurunglaxmi891@gmail.com>
2024-12-12 14:15:36 +0545

    stepDefinition login-context update

--- tests/stepDefinitions/loginContext.js
+++ tests/stepDefinitions/loginContext.js
@@ -1,16 +1,21 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 
-Given('the user has navigated to the login page', function () {
+Given('the user "admin" has navigated to the login page', function () {
     // Write code here that turns the phrase above into concrete actions
-    console.log("hello");
+    console.log("given");
   });
 
-  When('user logs in with email {string} and password {string}', function (string, string2) {
+  When('user "admin" logs in with email {string} and password {string}', function (string, string2) {
     // Write code here that turns the phrase above into concrete actions
-    console.log("hello");
+    console.log("when");
   });
 
   Then('user should be navigated to home page', function () {
     // Write code here that turns the phrase above into concrete actions
-    console.log("hello");
+    console.log("then");
+  });
+
+  Then('error message {string} should be shown', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    console.log("then");
   });
