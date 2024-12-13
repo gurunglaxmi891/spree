45b28520d51a72d428b41af33d77a0555bd59db4
gurunglaxmi891 <gurunglaxmi891@gmail.com>
2024-12-13 16:15:20 +0545

    login feature scenario and login context update

--- tests/features/login.feature
+++ tests/features/login.feature
@@ -1,31 +1,29 @@
-Feature: login
-  As an admin
-  I want to login to the admin panel
-  So that I can manage my products
+Feature: login 
+    As an admin
+    I want to log in to the website admin panel
+    So that I can manage my products
 
-  Background:
-    Given the user "admin" has navigated to the login page
+    Background: 
+        Given user 'admin' has navigated to the admin login page
 
-  Scenario: login with valid credentials
-    When user "admin" logs in with email "admin@admin.com" and password "admin123456"
-    Then user should be navigated to home page
 
-  Scenario: login with invalid credentials
-    When user "admin" logs in with email "admin@admin.com" and password "123"
-    Then error message "Invalid Credentials" should be shown
+    Scenario: login with valid credentials
+        When user 'admin' logs in with following credentials
+            | email           | password    |
+            | admin@admin.com | admin123456 | 
+        Then user 'admin' should be navigated to admin panel dashboard
 
-  Scenario Outline: login with invalid email format
-    When the user enters email address as "<invalidemail>" and "<password>"
-    Then the error "<message>" should pop
-    Examples:
-        | invalidemail | password | message         |
-        | ab.com       | hello    | @ is missing    |
-        | abc@gmail    | hiii     | .com is missing |
 
-  Scenario: credentials are missing
-    When the user enters following information
-        | email      | password |
-        |            | hello    |
-        |a@admin.com |          |
-    Then error message "Fill all details" should pop
-    
+   Scenario Outline: login with invalid credentials
+        When the user tries to log in with following credentials
+            | email   | password   |
+            | <email> | <password> |
+        Then error message "<errorMessage>" should be shown
+        Examples:
+            | email            | password       | errorMessage               |
+            | admin@admin.com  | abc            | Invalid email or password. |
+            | xyz@gmail.com    | hashda         | Invalid email or password. |
+            | admin@admin.com  |                | Invalid email or password. |
+            |                  | klaslkda       | Invalid email or password. |
+            |                  |                | Invalid email or password. |
+            | admin            | adminadmin123  | Manage Passwords           |
--- tests/stepDefinitions/loginContext.js
+++ tests/stepDefinitions/loginContext.js
@@ -1,44 +1,28 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 
-Given('the user "admin" has navigated to the login page', function () {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("given");
-  });
+Given('user {string} has navigated to the admin login page', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("given");
+});
 
-  When('user "admin" logs in with email {string} and password {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("when");
-  });
+When('user {string} logs in with following credentials', function (string, dataTable) {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("when");
+});
 
-  When('the user enters email address as {string} and {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("when");
-  });
+Then('user {string} should be navigated to admin panel dashboard', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("then");
+});
 
-  When('the user enters following information', function (dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    for(const {email,password} of dataTable.hashes()){
-      console.log(email, password);
-    }
-  });
+When('the user tries to log in with following credentials', function (dataTable) {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("when");
+});
 
-  Then('user should be navigated to home page', function () {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("then");
-  });
+Then('error message {string} should be shown', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  console.log("then");
+});
 
-  Then('error message {string} should be shown', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("then");
-  });
-
-  Then('the error {string} should pop', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("then");
-  });
-
-  Then('error message {string} should pop', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log("then");
-  });
 
